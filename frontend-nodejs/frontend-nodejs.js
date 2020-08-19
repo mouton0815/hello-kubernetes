01c722fdd8ebe288a6b65e73c530f28ef598d380
Torsten Schlieder <torsten.schlieder@webfleet.com>
2020-08-19 15:12:36 +0200

    Simplified service frontend-nodejs

--- frontend-nodejs/frontend-nodejs.js
+++ frontend-nodejs/frontend-nodejs.js
@@ -2,36 +2,21 @@ const express = require('express')
 const axios = require('axios').default
 
 const PORT = 8080
-const BACKEND_URL1 = process.env.BACKEND_URL1 || 'backend-golang'
-const BACKEND_URL2 = process.env.BACKEND_URL2 || 'backend-spring'
+
+const BACKEND_HOSTS = [
+    process.env.BACKEND_HOST1 || 'backend-spring',
+    process.env.BACKEND_HOST2 || 'backend-golang'
+]
 
 const app = express()
-
-async function queryBackend(name) {
-    const target1 = `http://${BACKEND_URL1}/${name}`
-    const target2 = `http://${BACKEND_URL2}/${name}`
-    console.log(`Call target URLs ${target1} ${target2}`)
-    const promises = [axios.get(target1), axios.get(target2)]
-    const responses = await Promise.all(promises)
-    return responses.reduce((acc, result) => acc + result.data + '\n', '')
-}
-
-async function startupProbe() {
-    console.log(`Backend URLs: ${BACKEND_URL1} ${BACKEND_URL2}, perform connection test ...`)
-    try {
-        const result = await queryBackend('Probe')
-        console.log(`Backend(s) answered with\n${result}\nProbe ok`)
-    } catch (e) {
-        console.warn(`Backend(s) returned error ${e.message}, continue anyway`)
-    }
-}
 
 app.get('*', async (req, res) => {
     console.log(req.method, req.url)
     const name = req.url.substr(1)
     try {
-        const result = await queryBackend(name)
-        res.send(`${result}[NODEJS] Hello ${name}`)
+        const results = await proxyToBackends(BACKEND_HOSTS, name)
+        const joined = results.reduce((acc, result) => `${acc}\n${result}`, `[NODEJS] Hello ${name}`)
+        res.send(joined)
     } catch (e) {
         console.error(e)
         res.status(400).send(e)
@@ -40,5 +25,14 @@ app.get('*', async (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server listens on port ${PORT}`)
-    startupProbe()
 })
+
+async function proxyToBackends(hosts, name) {
+    const urls = hosts.map(host => `http://${host}/${name}`)
+    console.log(`Call target URLs ${urls}`)
+    const promises = urls.map(url => axios.get(url))
+    const responses = await Promise.all(promises)
+    const results = responses.map(response => response.data)
+    console.log(`Retrieved results ${results}`)
+    return results
+}

eb10472eb1e9ee6134c69f9af2d5edeaaf6f1a47
Torsten Schlieder <torsten.schlieder@unite.eu>
2025-01-31 16:57:56 +0100

    Multiple attempts to connect to Redis

--- backend-rust/src/main.rs
+++ backend-rust/src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, thread, time};
 use std::sync::{Arc, Mutex};
 use axum::extract::{Path,State};
 use axum::http::StatusCode;
@@ -19,10 +19,7 @@ async fn main() {
         Err(_) => REDIS_HOST.to_string()
     };
     println!("Redis host: {}", redis_host);
-    let mut redis = RedisClient::new("redis://127.0.0.1/").unwrap();
-    let value = redis.inc("foo").unwrap();
-    println!("Redis result: {}", value);
-
+    let redis = RedisClient::new("redis://127.0.0.1/").unwrap();
     spawn_http_server(Arc::new(Mutex::new(redis))).await;
 }
 
@@ -36,8 +33,6 @@ async fn spawn_http_server(state: MutexSharedState) {
     axum::serve(listener, app).await.unwrap()
 }
 
-
-//  message := "[RUSTIC] " + h.greetingLabel + " " + name + " (call #" + count + ")"
 async fn rest_handler(State(state): State<MutexSharedState>, Path(path): Path<String>) -> Result<String, StatusCode> {
     let greeting_label = "#greetingLabel#"; // TODO: Move to env
     let mut guard = state.lock().unwrap();
@@ -54,8 +49,25 @@ struct RedisClient {
 impl RedisClient {
     fn new(address: &str) -> RedisResult<Self> {
         let client = redis::Client::open(address)?;
-        let conn = client.get_connection()?;
-        Ok(Self { conn })
+        // Redis may not be up and running yet, so try at most three times to connect
+        let mut attempts = 1;
+        loop {
+            match client.get_connection() {
+                Ok(conn) => {
+                    println!("Connection to Redis established - attempt {}", attempts);
+                    return Ok(Self { conn })
+                }
+                Err(err) => {
+                    println!("{} - attempt {}", err.to_string(), attempts); // TODO: Error log
+                    if attempts >= 3 {
+                        return Err(err)
+                    }
+                }
+            }
+            let duration = time::Duration::from_secs(3);
+            thread::sleep(duration);
+            attempts += 1;
+        }
     }
 
     fn inc(&mut self, key: &str) -> RedisResult<u32> {

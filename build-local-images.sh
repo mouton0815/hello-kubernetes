824dde878dfd0ac1ef4636a080b9b3df21e6ff03
Torsten Schlieder <torsten.schlieder@unite.eu>
2025-02-28 18:25:25 +0100

    Shorter syntax

--- build-local-images.sh
+++ build-local-images.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Build images for local testing
-docker image build --tag mouton4711/hello_k8s_backend-rust ./backend-rust
-docker image build --tag mouton4711/hello_k8s_backend-spring ./backend-spring
-docker image build --tag mouton4711/hello_k8s_backend-golang ./backend-golang
-docker image build --tag mouton4711/hello_k8s_frontend-nodejs ./frontend-nodejs
+docker build -t mouton4711/hello_k8s_backend-rust ./backend-rust
+docker build -t mouton4711/hello_k8s_backend-spring ./backend-spring
+docker build -t mouton4711/hello_k8s_backend-golang ./backend-golang
+docker build -t mouton4711/hello_k8s_frontend-nodejs ./frontend-nodejs
